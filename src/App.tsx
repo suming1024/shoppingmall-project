8e5fbc63344cfe5166e5aa7eff44c22099130c88
suming1024 <jungsuming1024@kakao.com>
2026-04-15 22:00:46 +0900

    shopping

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
   }
 
   return (
-    <BrowserRouter basename='/shopping/'>
+    <BrowserRouter basename='/shoppingmall-project/'>
       <Header
         isLoggedIn = {isLoggedIn}
         userId = {userId}
